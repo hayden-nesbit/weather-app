f2b74d2baee7081bb660e6fd8ef71d08edae39c7
Hayden Nesbit <hayden.nesbit@campusoutreach.org>
2020-03-04 15:16:40 -0500

    complete

--- JS/main.js
+++ JS/main.js
@@ -5,30 +5,44 @@ document.getElementById("main wrapper").style.display = "none";
 let kelvin = 0;
 
 function getAPI() {
-    document.getElementById("main wrapper").style.display = "block";
 
     fetch("https://api.openweathermap.org/data/2.5/weather?zip=" + zip.value + ",&appid=be1fc406944f287cb38a91655d63aa8d")
         .then((response) => {
+            if (!response.ok) {
+                throw response
+            }
             let result = response.json();
             return result;
         })
-
         .then((data) => {
-            console.log(data)
-            let temp = data.main.temp;
-            kelvin = temp;
+
+            // if (data.hasOwnProperty('cod')) {
+            //     alert(data.message)
+
+            // } else {
+            document.getElementById("main wrapper").style.display = "block";
+            document.getElementById("city").innerHTML = data.name;
+            document.getElementById("condition").innerHTML = data.weather[0].main;
+            document.getElementById("pic").setAttribute("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
+            document.getElementById("temp").innerHTML = Math.floor(data.main.temp) + " K";
+           
+
+            kelvin = data.main.temp;
             let far = (kelvin - 273.15) * (9 / 5) + 32;
             let celcius = (far - 32) * (5 / 9);
-            let city = data.name;
-            let condition = data.weather[0].main;
-            let icon = data.weather[0].icon;
+        })
 
-            document.getElementById("city").innerHTML = city;
-            document.getElementById("condition").innerHTML = condition;
-            document.getElementById("temp").innerHTML = Math.floor(temp) + " K";
-            document.getElementById("pic").setAttribute("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png");
+        //I have no idea how to explain this... Ian did it
+        .catch((error) => {
+            console.log(error)
+            error.text().then(errMessage => {
+                errMessage = JSON.parse(errMessage)
+                alert(errMessage.message)
+            })
 
-        });
+
+
+        })
 }
 
 let view = 0;
@@ -37,6 +51,7 @@ function change() {
     let far = (kelvin - 273.15) * (9 / 5) + 32;
     let celcius = (far - 32) * (5 / 9);
     switch (view) {
+
         case 1:
             document.getElementById("temp").innerHTML = Math.floor(celcius) + " C";
             break;
