de71dc68659e94c3e2ed426f29f5b6cb4c648e0e
Hayden Nesbit <hayden.nesbit@campusoutreach.org>
2020-03-03 20:56:44 -0500

    value

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,9 @@
 //api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={be1fc406944f287cb38a91655d63aa8d}
 
 let zip = document.getElementById("inputZip");
-console.log(zip)
+//console.log(zip)
 
 function getAPI() {
-    fetch("api.openweathermap.org/data/2.5/weather?zip=" + zip + ",&appid=be1fc406944f287cb38a91655d63aa8d");
+    fetch("api.openweathermap.org/data/2.5/weather?zip=" + zip.value + ",&appid=be1fc406944f287cb38a91655d63aa8d");
     return Response
 }
