0a55c0541b12256ef108b8f812551f403f69c18c
Hayden Nesbit <hayden.nesbit@campusoutreach.org>
2020-03-04 11:59:25 -0500

    working button

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,12 @@
 let zip = document.getElementById("inputZip");
 
+document.getElementById("main wrapper").style.display = "none";
+
+let kelvin = 0;
+
 function getAPI() {
+    document.getElementById("main wrapper").style.display = "block";
+
     fetch("https://api.openweathermap.org/data/2.5/weather?zip=" + zip.value + ",&appid=be1fc406944f287cb38a91655d63aa8d")
         .then((response) => {
             let result = response.json();
@@ -10,19 +16,42 @@ function getAPI() {
         .then((data) => {
             console.log(data)
             let temp = data.main.temp;
+            kelvin = temp;
+            let far = (kelvin - 273.15) * (9 / 5) + 32;
+            let celcius = (far - 32) * (5 / 9);
             let city = data.name;
             let condition = data.weather[0].main;
             //let image = data.
-            document.getElementById("temp").innerHTML = Math.floor(temp);
+
             document.getElementById("city").innerHTML = city;
             document.getElementById("condition").innerHTML = condition;
-        
+            document.getElementById("temp").innerHTML = Math.floor(temp) + " K";
+
         });
-
-        
-
 
 
     //add catch
+}
+
+let view = 0;
+function change() {
+
+    let far = (kelvin - 273.15) * (9 / 5) + 32;
+    let celcius = (far - 32) * (5 / 9);
+    switch (view) {
+        case 1:
+            document.getElementById("temp").innerHTML = Math.floor(celcius) + " C";
+            break;
+
+        case 2:
+            document.getElementById("temp").innerHTML = Math.floor(far) + " F";
+            break;
+
+    }
+}
+
+function nextView() {
+    view++;
+    change();
 }
 
